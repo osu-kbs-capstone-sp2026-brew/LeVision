9312676d93dc0cfd657952362340ab32ef62d0cf
vihaaaaan <vihaanv14@gmail.com>
2026-04-27 03:49:38 -0400

    increase upload size limit from 300mb to 5gb

--- levision-web/next.config.ts
+++ levision-web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   experimental: {
-    proxyClientMaxBodySize: '300mb',
+    proxyClientMaxBodySize: '5gb',
   },
   images: {
     remotePatterns: [
